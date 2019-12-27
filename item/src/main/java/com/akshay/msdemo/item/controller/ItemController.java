d4d7fef4f9afba414a09ba97eaaa3f03501f7710
Akshay Nande <akshaytnande@gmail.com>
2019-12-27 14:27:08 -0600

    Added disricbuted tracomg using Zipkin, Sleuth and Rabbit MQ and added fault tolerance to Item Service using Hystrix

--- item/src/main/java/com/akshay/msdemo/item/controller/ItemController.java
+++ item/src/main/java/com/akshay/msdemo/item/controller/ItemController.java
@@ -2,6 +2,7 @@ package com.akshay.msdemo.item.controller;
 
 import com.akshay.msdemo.item.model.Item;
 import com.akshay.msdemo.item.service.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -32,14 +33,21 @@ public class ItemController {
     }
 
     @GetMapping("/{name}")
+//    @HystrixCommand(fallbackMethod = )
     public Item findItemByName(@PathVariable("name") String name) {
         Item itemWithName = itemService.getItemByName(name);
         return  itemWithName;
     }
 
-//    @GetMapping("/items/{itemsList}")
-//    public List<Item> getItemsByList(@PathVariable("itemsList") List<String> itemsList) {
-//        return itemService.findByNamesList(itemsList);
-//    }
+    @GetMapping("/fault-tolerance-example")
+    @HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+    public Item retrieveConfiguration() {
+        throw new RuntimeException("Not available");
+    }
+
+    public Item fallbackRetrieveConfiguration() {
+        Item dummyItem = new Item("dummy name","dummy description", 999.99);
+        return dummyItem;
+    }
 
 }
