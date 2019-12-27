e533a9bb6a62538149b6ed54a70872ca04e4b701
Akshay Nande <akshaytnande@gmail.com>
2019-12-26 22:14:47 -0600

    setup Netflix Eureka Naming Server for service registration and service discovery

--- sales-service/src/main/java/com/akshay/msdemo/salesservice/SalesServiceApplication.java
+++ sales-service/src/main/java/com/akshay/msdemo/salesservice/SalesServiceApplication.java
@@ -2,10 +2,13 @@ package com.akshay.msdemo.salesservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
 @EnableFeignClients("com.akshay.msdemo.salesservice")
+@EnableDiscoveryClient
 public class SalesServiceApplication {
 
     public static void main(String[] args) {
